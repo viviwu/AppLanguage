7a292e967d35c0b84a1e195e106393716a4c4f9b
qway <vivi705wu@sina.com>
2014-12-24 17:53:47 +0800

    Initial Commit

---
+++ Language/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Language
+//
+//  Created by qway on 14/12/24.
+//  Copyright (c) 2014年 qway. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "viviAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([viviAppDelegate class]));
+    }
+}
---
+++ LanguageTests/LanguageTests.m
@@ -0,0 +1,34 @@
+//
+//  LanguageTests.m
+//  LanguageTests
+//
+//  Created by qway on 14/12/24.
+//  Copyright (c) 2014年 qway. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LanguageTests : XCTestCase
+
+@end
+
+@implementation LanguageTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
